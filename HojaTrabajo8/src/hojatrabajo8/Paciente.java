ef800741f78cace067e48452f0360a7a3f5f5bb9
Unknown <anl17149@uvg.edu.gt>
2018-03-23 11:19:58 -0600

    Clase paciente
    Se creó la clase paciente para modelarlos en el programa y poder identificar a cada uno en lugar de sólo usar el tipo

---
+++ HojaTrabajo8/src/hojatrabajo8/Paciente.java
@@ -0,0 +1,64 @@
+package hojatrabajo8;
+/**
+ * Clase que se utiliza para modelar el paciente. Sus atributos son el nombre, enfermedad, y tipo de paciente. 
+ * @author Javier Anleu - 17149
+ * @author Andres Urizar - 17632
+ * @since March 23, 2018
+ * @version 1.0
+ *
+ */
+public class Paciente {
+	
+	private String nombre;
+	private String enfermedad;
+	private String tipo;
+	/**
+	 * @param nombre
+	 * @param enfermedad
+	 * @param tipo
+	 */
+	public Paciente(String nombre, String enfermedad, String tipo) {
+		this.nombre = nombre;
+		this.enfermedad = enfermedad;
+		this.tipo = tipo;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @return the enfermedad
+	 */
+	public String getEnfermedad() {
+		return enfermedad;
+	}
+	/**
+	 * @return the tipo
+	 */
+	public String getTipo() {
+		return tipo;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @param enfermedad the enfermedad to set
+	 */
+	public void setEnfermedad(String enfermedad) {
+		this.enfermedad = enfermedad;
+	}
+	/**
+	 * @param tipo the tipo to set
+	 */
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+
+}
